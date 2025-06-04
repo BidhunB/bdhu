0c9edf488738f173be0f4849c5e860a0846ec24b
BidhunB <bidhunbibin05@gmail.com>
2025-06-04 21:43:50 +0530

    Add certificates and contact pages, implement responsive navbar, and update main page layout
    - Created `Certificates` and `Contact` components with basic structure.
    - Implemented a responsive `NavbarDemo` component with navigation links.
    - Updated main page to include a dynamic navbar and background images.
    - Added utility functions for class name merging.
    - Updated `package.json` and `package-lock.json` to include new dependencies.
    - Added Tailwind CSS configuration.
    - Updated TypeScript configuration for base URL.
    - Added new background images and removed unused SVG files.

---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+contacttt
+    </div>
+  )
+}
+
+export default Contact
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+  ],
+  // ...rest of your config
+}
